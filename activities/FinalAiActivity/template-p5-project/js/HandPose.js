c18ccb0031afbf13876c4f2e4e713c307d74538b
NicholasL3duc <nicholas.leduc1@gmail.com>
2024-02-27 13:40:55 -0500

    I AM A GENIUS
    i got the placement of the whale to work, i just need to adjust to the fliped camera postioning to the whale

--- activities/FinalAiActivity/template-p5-project/js/HandPose.js
+++ activities/FinalAiActivity/template-p5-project/js/HandPose.js
@@ -1,110 +1,44 @@
-/**
+let handpose;
+let video;
+let hands = [];
 
-Handpose Framework
-Pippin Barr
-
-A skeleton framework for using ml5.js's Handpose feature. Includes a
-loading screen followed by a live webcam feed with a circle drawn at
-the tip of the user's index finger.
-
-*/
-class Handpose {
-
-"use strict";
-
-// Current state of program
-state = `loading`; // loading, running
-// User's webcam
-video;
-// The name of our model
-modelName = `Handpose`;
-// Handpose object (using the name of the model for clarity)
-handpose;
-// The current set of predictions made by Handpose once it's running
-predictions = [];
-
-/**
-Starts the webcam and the Handpose
-*/
-setup() {
+function setup() {
   createCanvas(640, 480);
-
-  // Start webcam and hide the resulting HTML element
   video = createCapture(VIDEO);
+  video.size(width, height);
+
+  handpose = ml5.handpose(video, modelReady);
+
+  // This sets up an event that fills the global variable "predictions"
+  // with an array every time new hand poses are detected
+  handpose.on("hand", results => {
+    hands = results;
+  });
+
+  // Hide the video element, and just show the canvas
   video.hide();
-
-  // Start the Handpose model and switch to our running state when it loads
-  handpose = ml5.handpose(video, {
-    flipHorizontal: true
-  }, function() {
-    // Switch to the running state
-    state = `running`;
-  });
-
-  // Listen for prediction events from Handpose and store the results in our
-  // predictions array when they occur
-  handpose.on(`predict`, function(results) {
-    predictions = results;
-  });
 }
 
-/**
-Handles the two states of the program: loading, running
-*/
-draw() {
-  if (state === `loading`) {
-    loading();
+function modelReady() {
+  console.log("Model ready!");
+}
+
+function draw() {
+  image(video, 0, 0, width, height);
+
+  // We can call both functions to draw all keypoints and the skeletons
+  drawKeypoints();
+}
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints() {
+  for (let i = 0; i < hands.length; i += 1) {
+    const hand = hands[i];
+    for (let j = 0; j < hand.landmarks.length; j += 1) {
+      const keypoint = hand.landmarks[j];
+      fill(0, 255, 0);
+      noStroke();
+      ellipse(keypoint[0], keypoint[1], 10, 10);
+    }
   }
-  else if (state === `running`) {
-    running();
-  }
-}
-
-/**
-Displays a simple loading screen with the loading model's name
-*/
-loading() {
-  background(255);
-
-  push();
-  textSize(32);
-  textStyle(BOLD);
-  textAlign(CENTER, CENTER);
-  text(`Loading ${modelName}...`, width / 2, height / 2);
-  pop();
-}
-
-/**
-Displays the webcam.
-If there is a hand it outlines it and highlights the tip of the index finger
-*/
-running() {
-  // Display the webcam with reveresd image so it's a mirror
-  this.flippedVideo = ml5.flipImage(video);
-  image(flippedVideo, 0, 0, width, height);
-
-  // Check if there currently predictions to display
-  if (predictions.length > 0) {
-    // Technically there will only be ONE because it only detects ONE hand
-    // Get the hand predicted
-    this.hand = predictions[0];
-    // Highlight it on the canvas
-    highlightHand(hand);
-  }
-}
-
-/**
-Provided with a detected hand it highlights the tip of the index finger
-*/
- highlightHand(hand) {
-  // Display a circle at the tip of the index finger
-  this.index = hand.annotations.indexFinger[3];
-  this.indexX = index[0];
-  this.indexY = index[1];
-  push();
-  fill(255, 255, 0);
-  noStroke();
-  ellipse(indexX, indexY, 50);
-  pop();
-}
 }
--- activities/TestAIProject/js/script.js
+++ activities/TestAIProject/js/script.js
@@ -1,93 +1,104 @@
-/**
- * Title of Project
- * Author Name
- *
- * This is a template. You must fill in the title, author,
- * and this description to match your project!
- */
+let handpose;
+let video;
+let pointerX, pointerY, knuckle, ring;
 
-"use strict";
 // the whale let
 let whale = {
-  x: 250,
-  y: 250,
-  size: 150,
-  vx: 0,
-  vy: 0,
-  speed: 3.5,
-};
+    x: 250,
+    y: 250,
+    size: 150,
+    vx: 0,
+    vy: 0,
+    speed: 3.5,
+  };
+  
+  let state = "title";
+  
+  // the images into let
+  let imgWhale;
+  let imgTrash;
+  let imgFish;
+  let imgDead;
+  let imghappy;
+  
+  // the arrays used
+  let hands = [];
+  let school = [];
+  let garbage = [];
 
-let state = "title";
-
-// the images into let
-let imgWhale;
-let imgTrash;
-let imgFish;
-let imgDead;
-let imghappy;
-
-// the arrays used
-let school = [];
-let garbage = [];
-function preload() {
-  // all of the images used
-  imgWhale = loadImage(`assets/images/whale.png`);
-  imgTrash = loadImage(`assets/images/trash.png`);
-  imgFish = loadImage(`assets/images/fish.webp`);
-  imgDead = loadImage(`assets/images/deadWhale.webp`);
-  imghappy = loadImage(`assets/images/happy.webp`);
-}
-function title() {
-  push();
-  textSize(45);
-  fill(140, 50, 100);
-  textAlign(CENTER, CENTER);
-  text(
-    "Eat All The Fish And Avoid The Trash!!!",
-
-    width / 2,
-    height / 2
-  );
-
-  pop();
-}
-function win() {
-  push();
-  textSize(64);
-  fill(255, 150, 150);
-  textAlign(CENTER, CENTER);
-  text("Yummy!", width / 2, height / 2);
-  text("You Won, Congrats", width / 2, height / 1.5);
-  image(imghappy, 600, 50, 200, 200);
-  pop();
-}
-function loss() {
-  push();
-  textSize(40);
-  fill(189, 38, 21);
-  textAlign(CENTER, CENTER);
-  text("Yucky!! :(", width / 2, height / 2);
-  fill(190, 50, 50);
-  text("you lose try again", width / 2, height / 1.5);
-  image(imgDead, 600, 100, 300, 300);
-
-  pop();
-}
+  function preload() {
+    // all of the images used
+    imgWhale = loadImage(`assets/images/whale.png`);
+    imgTrash = loadImage(`assets/images/trash.png`);
+    imgFish = loadImage(`assets/images/fish.webp`);
+    imgDead = loadImage(`assets/images/deadWhale.webp`);
+    imghappy = loadImage(`assets/images/happy.webp`);
+  }
+  function title() {
+    push();
+    textSize(45);
+    fill(140, 50, 100);
+    textAlign(CENTER, CENTER);
+    text(
+      "Eat All The Fish And Avoid The Trash!!!",
+  
+      width / 2,
+      height / 2
+    );
+  
+    pop();
+  }
+  function win() {
+    push();
+    textSize(64);
+    fill(255, 150, 150);
+    textAlign(CENTER, CENTER);
+    text("Yummy!", width / 2, height / 2);
+    text("You Won, Congrats", width / 2, height / 1.5);
+    image(imghappy, 600, 50, 200, 200);
+    pop();
+  }
+  function loss() {
+    push();
+    textSize(40);
+    fill(189, 38, 21);
+    textAlign(CENTER, CENTER);
+    text("Yucky!! :(", width / 2, height / 2);
+    fill(190, 50, 50);
+    text("you lose try again", width / 2, height / 1.5);
+    image(imgDead, 600, 100, 300, 300);
+  
+    pop();
+  }
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight);
+    //   array for fish
+    for (let i = 0; i < 6; i++) {
+      school[i] = createFish(random(0, width), random(0, height));
+    }
+  
+    //    array for trash
+    for (let g = 0; g < 4; g++) {
+      garbage[g] = createFish(random(0, width), random(0, height));
+    }
+  
+    pointerX = 50;
+    pointerY = windowHeight / 2;
 
-  //   array for fish
-  for (let i = 0; i < 6; i++) {
-    school[i] = createFish(random(0, width), random(0, height));
-  }
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  video.hide();
 
-  //    array for trash
-  for (let g = 0; g < 4; g++) {
-    garbage[g] = createFish(random(0, width), random(0, height));
-  }
+  handpose = ml5.handpose(video, modelReady);
 
-  whale.x = 50;
-  whale.y = windowHeight / 2;
+  // This sets up an event that fills the global variable "predictions"
+  // with an array every time new hand poses are detected
+  handpose.on("hand", results => {
+    hands = results;
+  });
+
+  // Hide the video element, and just show the canvas
+  video.hide();
 }
 
 // Creation of the fish and trash
@@ -133,8 +144,15 @@ function createUser() {
   );
   pop();
 }
+ 
+
+function modelReady() {
+  console.log("Model ready!");
+}
 
 function draw() {
+  image(video, 0, 0, width, height);
+
   background(0, 0, 139);
   // all the states
   if (state === "title") {
@@ -147,132 +165,143 @@ function draw() {
     loss();
   }
   // constraint the whale/user to the canvas
-  whale.x = constrain(whale.x, 0, windowWidth);
-  whale.y = constrain(whale.y, 0, windowHeight);
+  pointerX = constrain(pointerX, 0, windowWidth);
+  pointerY = constrain(pointerY, 0, windowHeight);
+  // We can call both functions to draw all keypoints and the skeletons
+  drawKeypoints();
+
+  Simulation();
 }
-function simulation() {
-  moveWhale(whale);
-  for (let fish of school) {
-    moveFish(fish);
-    displayFish(fish);
+
+function Simulation() {
+//   moveWhale(this.whale);
+for (fish of school) {
+    moveFish(this.fish);
+    displayFish(this.fish);
     overlapCheckFish();
     displayWhale();
+    // moveWhale(whale);
   }
-  for (let trash of garbage) {
+  for (this.trash of garbage) {
     moveTrash(trash);
     displayTrash(trash);
     overlapCheckTrash();
   }
+
 }
 function moveFish(fish) {
-  // Choose whether to change direction
-  let change = random(0, 1);
-  if (change < 0.05) {
-    fish.vx = random(-fish.speed, fish.speed);
-    fish.vy = random(-fish.speed, fish.speed);
+    // Choose whether to change direction
+    let change = random(0, 1);
+    if (change < 0.05) {
+      fish.vx = random(-fish.speed, fish.speed);
+      fish.vy = random(-fish.speed, fish.speed);
+    }
+  
+    // Move the fish
+    fish.x = fish.x + fish.vx;
+    fish.y = fish.y + fish.vy;
+  
+    // Constrain the fish to the canvas
+  
+    fish.x = constrain(fish.x, 0, width);
+    fish.y = constrain(fish.y, 0, height);
   }
-
-  // Move the fish
-  fish.x = fish.x + fish.vx;
-  fish.y = fish.y + fish.vy;
-
-  // Constrain the fish to the canvas
-
-  fish.x = constrain(fish.x, 0, width);
-  fish.y = constrain(fish.y, 0, height);
-}
-function moveTrash(trash) {
-  let change = random(0, 1);
-  if (change < 0.05) {
-    trash.vx = random(-trash.speed, trash.speed);
-    trash.vy = random(-trash.speed, trash.speed);
+  function moveTrash(trash) {
+    let change = random(0, 1);
+    if (change < 0.05) {
+      trash.vx = random(-trash.speed, trash.speed);
+      trash.vy = random(-trash.speed, trash.speed);
+    }
+  
+    // Move the trash
+    trash.x = trash.x + trash.vx;
+    trash.y = trash.y + trash.vy;
+  
+    // Constrain the trash to the canvas
+  
+    trash.x = constrain(trash.x, 0, width);
+    trash.y = constrain(trash.y, 0, height);
   }
-
-  // Move the trash
-  trash.x = trash.x + trash.vx;
-  trash.y = trash.y + trash.vy;
-
-  // Constrain the trash to the canvas
-
-  trash.x = constrain(trash.x, 0, width);
-  trash.y = constrain(trash.y, 0, height);
-}
-function moveWhale(whale) {
-  if (keyIsDown(87)) {
-    //moving using the W key
-    whale.vy = -whale.speed;
-  } else if (keyIsDown(83)) {
-    //moving using the A key
-    whale.vy = whale.speed;
-  } else {
-    whale.vy = 0;
+ 
+  
+  function displayFish(fish) {
+    push();
+    // display for the fish
+    image(imgFish, fish.x, fish.y, fish.size, fish.size);
+    pop();
   }
+ function displayWhale() {
+    // display for whale
+    push();
+    //  image(imgWhale,keypoint[0], keypoint[1], whale.size, whale.size);
+    pop();
 
-  if (keyIsDown(68)) {
-    // moving using the S key
-    whale.vx = whale.speed;
-  } else if (keyIsDown(65)) {
-    // moving using the D key
-    whale.vx = -whale.speed;
-  } else {
-    whale.vx = 0;
   }
-  whale.x += whale.vx;
-  whale.y += whale.vy;
-}
-function displayWhale() {
-  // display for whale
-  push();
-  image(imgWhale, whale.x, whale.y, whale.size, whale.size);
-  pop();
+  function displayTrash(trash) {
+    // displaying the trash
+    push();
+    image(imgTrash, trash.x, trash.y, trash.size, trash.size);
+    pop();
+  }
+  
+  function overlapCheckFish() {
+    image(imgWhale,pointerX,pointerY, whale.size, whale.size);
+
+    // over lap for the fishes from the whale
+    for (let fish of school) {
+       let s1 = dist(pointerX, pointerY, fish.x, fish.y);
+       if (s1 < whale.size / 2 + fish.size / 2) {
+         for (let f = 0; f < school.length; f++) {
+           let fish = school[f];
+           let d = dist(pointerX, pointerY, fish.x, fish.y);
+           if (d < fish.size / 2 + whale.size / 2) {
+             school.splice(f, 1);
+           }
+         }
+       }
+     }
+     if (school.length === 0) {
+       state = `win`;
+     }
 }
 
-function displayFish(fish) {
-  push();
-  // display for the fish
-  image(imgFish, fish.x, fish.y, fish.size, fish.size);
-  pop();
-}
 
-function displayTrash(trash) {
-  // displaying the trash
-  push();
-  image(imgTrash, trash.x, trash.y, trash.size, trash.size);
-  pop();
-}
 
-function overlapCheckFish() {
-  // over lap for the fishes from the whale
-  for (let fish of school) {
-    let s1 = dist(whale.x, whale.y, fish.x, fish.y);
-    if (s1 < whale.size / 2 + fish.size / 2) {
-      for (let f = 0; f < school.length; f++) {
-        let fish = school[f];
-        let d = dist(whale.x, whale.y, fish.x, fish.y);
-        if (d < fish.size / 2 + whale.size / 2) {
-          school.splice(f, 1);
-        }
+
+   
+  
+  function overlapCheckTrash() {
+    // overlap for the trash on the whale
+    for (let trash of garbage) {
+      let g1 = dist(pointerX, pointerY, trash.x, trash.y);
+      if (g1 < whale.size / 2 + trash.size / 2) {
+        state = "loss";
       }
     }
   }
-  if (school.length === 0) {
-    state = `win`;
-  }
-}
+  
+ 
 
-function overlapCheckTrash() {
-  // overlap for the trash on the whale
-  for (let trash of garbage) {
-    let g1 = dist(whale.x, whale.y, trash.x, trash.y);
-    if (g1 < whale.size / 2 + trash.size / 2) {
-      state = "loss";
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints() {
+  for (let i = 0; i < hands.length; i += 1) {
+    const hand = hands[i];
+    for (let j = 0; j < hand.landmarks.length; j += 1) {
+      const keypoint = hand.landmarks[j];
+      fill(0, 255, 0);
+      noStroke();
+if (j == 8) {
+    pointerX = keypoint[0];
+    pointerY = keypoint[1];
+    // print(keyTest)
+} else 
+if(j == 14) {
+    knuckle = keypoint[1];
+} else
+ if (j == 16 ){
+    ring = keypoint[1];
+ }
     }
-  }
 }
-
-// transition from title to sim
-function mousePressed() {
-  if (state === "title") {
-    state = "simulation";
-  }
 }
+// keypoint[0], keypoint[1],
