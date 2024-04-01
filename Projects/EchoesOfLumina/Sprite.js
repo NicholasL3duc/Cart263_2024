7bc9a1f410395ce188f048319e42d68cf7ac8a13
NicholasL3duc <nicholas.leduc1@gmail.com>
2024-04-01 13:50:24 -0400

    working sprite
    commiting before i start moving parts from my overworld class into my sprites

--- Projects/EchoesOfLumina/Sprite.js
+++ Projects/EchoesOfLumina/Sprite.js
@@ -10,7 +10,8 @@ class Sprite {
             [0,0],[1,0],[2,0],[3,0],
             ]
         }
-
+        this.currentAnimation = config.currentAnimation || "idleDown";
+        this.currentAnimationFrame = 0;
     }
 
 }
